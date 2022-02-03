48d01c7b57e50f9ba28c9347df68f8e52f855318
Jimin Kim <0305jimin@gmail.com>
2022-02-03 23:34:22 +0900

    design : style
    View, StyleSheet, ScrollView, Dimensions , flex

--- App.js
+++ App.js
@@ -1,15 +1,70 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
+import { StatusBar } from "expo-status-bar";
+import { View, Text, StyleSheet, ScrollView, Dimensions } from "react-native";
+const { width: SCREEN_WIDTH } = Dimensions.get("window");
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <View style={styles.city}>
+                <Text style={styles.cityName}>Seoul</Text>
+            </View>
+            <ScrollView
+                pagingEnabled
+                showsHorizontalScrollIndicator={false}
+                horizontal
+                contentContainerStyle={styles.weather}
+            >
+                <View style={styles.day}>
+                    <Text style={styles.temperature}>27</Text>
+                    <Text style={styles.description}>Sunny</Text>
+                </View>
+                <View style={styles.day}>
+                    <Text style={styles.temperature}>27</Text>
+                    <Text style={styles.description}>Sunny</Text>
+                </View>
+                <View style={styles.day}>
+                    <Text style={styles.temperature}>27</Text>
+                    <Text style={styles.description}>Sunny</Text>
+                </View>
+                <View style={styles.day}>
+                    <Text style={styles.temperature}>27</Text>
+                    <Text style={styles.description}>Sunny</Text>
+                </View>
+                <View style={styles.day}>
+                    <Text style={styles.temperature}>27</Text>
+                    <Text style={styles.description}>Sunny</Text>
+                </View>
+            </ScrollView>
+        </View>
+    );
 }
 
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "lightsteelblue",
+    },
+    city: {
+        flex: 1,
+        backgroundColor: "lightskyblue",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    cityName: {
+        fontSize: 68,
+        fontWeight: 500,
+    },
+    weather: {},
+    day: {
+        width: SCREEN_WIDTH,
+        alignItems: "center",
+    },
+    temperature: {
+        fontSize: 100,
+        marginTop: 50,
+    },
+    description: { fontSize: 50, marginTop: -20 },
+});
 const styles = StyleSheet.create({
   container: {
     flex: 1,
