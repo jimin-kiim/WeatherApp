2ec80ddc339e43d5f6c99c1eec0576bce09a7036
Jimin Kim <0305jimin@gmail.com>
2022-02-04 21:07:41 +0900

    !HOTFIX : hiding api key

--- App.js
+++ App.js
@@ -1,12 +1,42 @@
-
+import * as Location from "expo-location";
+import React, { useState, useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
+import { API_KEY } from "react-native-dotenv";
+// import { API_KEY } from "@env";
 import { View, Text, StyleSheet, ScrollView, Dimensions } from "react-native";
+
 const { width: SCREEN_WIDTH } = Dimensions.get("window");
+// console.log(API_KEY);
+
 export default function App() {
+    const [city, setCity] = useState("Loading...");
+    const [location, setLocation] = useState();
+    const [ok, setOk] = useState(true);
+    const ask = async () => {
+        const { granted } = await Location.requestForegroundPermissionsAsync();
+        if (!granted) {
+            setOk(false);
+        }
+        const {
+            coords: { latitude, longitude },
+        } = await Location.getCurrentPositionAsync({
+            accuracy: 5,
+        });
+        Location.setGoogleApiKey("");
+        const location = await Location.reverseGeocodeAsync(
+            { latitude, longitude },
+            { useGoogleMaps: false }
+        );
+        console.log(location);
+        setCity(location[0].city);
+    };
+    useEffect(() => {
+        ask();
+    }, []);
     return (
         <View style={styles.container}>
             <View style={styles.city}>
-                <Text style={styles.cityName}>Seoul</Text>
+                <Text style={styles.cityName}>{city}</Text>
             </View>
             <ScrollView
                 pagingEnabled
@@ -64,12 +94,4 @@ const styles = StyleSheet.create({
         marginTop: 50,
     },
     description: { fontSize: 50, marginTop: -20 },
-});
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
 });
