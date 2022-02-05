324e4de03dd6574ee55fd00446112b0c354eae7e
Jimin Kim <0305jimin@gmail.com>
2022-02-05 20:52:40 +0900

    design, feat: weather icon, date

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { StatusBar } from "expo-status-bar";
 import { GOOGLE_API, WEATER_API } from "./secrets.json";
 // import { API_KEY } from "react-native-dotenv";
 // import { API_KEY } from "@env";
+import { Ionicons } from "@expo/vector-icons";
 import {
     View,
     Text,
@@ -16,6 +17,19 @@ import {
 const { width: SCREEN_WIDTH } = Dimensions.get("window");
 // console.log(GOOGLE_API);
 
+const date = new Date().getDate();
+const month = new Date().toString().substring(4, 7);
+console.log(date, month);
+
+const icons = {
+    Clouds: "cloudy-outline",
+    Clear: "sunny-outline",
+    Rain: "rainy-outline",
+    Atmosphere: "partly-sunny-outline",
+    Snow: "snow-outline",
+    Drizzle: "rainy-outline",
+    Thunderstorm: "thunerstorm-outline",
+};
 export default function App() {
     const [city, setCity] = useState("Loading...");
     const [days, setDays] = useState([]);
@@ -41,7 +55,10 @@ export default function App() {
             `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=alerts&appid=${WEATER_API}&units=metric`
         );
         const json = await response.json();
+        console.log(json);
         setDays(json.daily);
+        console.log(new Date());
+        console.log();
     };
     useEffect(() => {
         ask();
@@ -67,16 +84,48 @@ export default function App() {
                     </View>
                 ) : (
                     days.map((day, index) => (
-                        <View key={index} style={styles.day}>
-                            <Text style={styles.temperature}>
-                                {parseFloat(day.temp.day).toFixed(1)}
-                            </Text>
-                            <Text style={styles.description}>
-                                {day.weather[0].main}
-                            </Text>
-                            <Text style={styles.tinyText}>
-                                {day.weather[0].description}
-                            </Text>
+                        <View key={index} style={styles.forecast}>
+                            <View>
+                                {(function () {
+                                    if (!index)
+                                        return (
+                                            <Text style={styles.date}>
+                                                Today
+                                            </Text>
+                                        );
+                                    if (index === 1)
+                                        return (
+                                            <Text style={styles.date}>
+                                                Tomorrow
+                                            </Text>
+                                        );
+                                    else
+                                        return (
+                                            <Text style={styles.date}>
+                                                {month} {date + index}
+                                            </Text>
+                                        );
+                                })()}
+                            </View>
+                            <View style={styles.day}>
+                                <View style={{ alignItems: "center" }}>
+                                    <Text style={styles.temperature}>
+                                        {parseFloat(day.temp.day).toFixed(1)}
+                                    </Text>
+
+                                    <Text style={styles.description}>
+                                        {day.weather[0].main}
+                                    </Text>
+                                    <Text style={styles.tinyText}>
+                                        {day.weather[0].description}
+                                    </Text>
+                                </View>
+                                <Ionicons
+                                    name={icons[day.weather[0].main]}
+                                    size={40}
+                                    color="black"
+                                />
+                            </View>
                         </View>
                     ))
                 )}
@@ -95,22 +144,34 @@ const styles = StyleSheet.create({
         backgroundColor: "lightskyblue",
         justifyContent: "center",
         alignItems: "center",
+        paddingTop: 20,
     },
     cityName: {
         fontSize: 68,
         fontWeight: "500",
     },
-    weather: {},
+    forecast: {
+        alignItems: "center",
+    },
+    date: {
+        fontSize: 30,
+        fontWeight: 200,
+        paddingTop: 50,
+    },
     day: {
         width: SCREEN_WIDTH,
-        alignItems: "center",
+        flexDirection: "row",
+        justifyContent: "center",
+        paddingTop: 40,
+        paddingLeft: 20,
     },
     temperature: {
         fontSize: 100,
         marginTop: 50,
     },
-    description: { fontSize: 50, marginTop: -20 },
+    description: { fontSize: 50, marginTop: 20, fontWeight: 300 },
     tinyText: {
         fontSize: 20,
+        fontWeight: 300,
     },
 });
